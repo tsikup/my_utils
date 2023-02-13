a77dd97200fcbc57c91e5751c5fc390a06823524
Nikos Tsiknakis <tsiknakisn@gmail.com>
2023-02-13 11:41:57 +0100

    create a telegram notification class

--- utils/chat.py
+++ utils/chat.py
@@ -1,29 +1,40 @@
 import socket
 
 
+class TelegramNotification:
+    def __init__(self, token, chat_id):
+        self.token = token
+        self.chat_id = chat_id
+
+    def send(self, message):
+        send_noti_to_telegram(message, self.token, self.chat_id)
+
+
 def send_noti_to_telegram(message, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID):
     try:
         import telegram
     except ImportError:
-        print('telegram package is not installed. ' +
-              'please install the package by executing $ pip install python-telegram-bot')
+        print(
+            "telegram package is not installed. "
+            + "please install the package by executing $ pip install python-telegram-bot"
+        )
         return
     except Exception as e:
-        print('sending message failed', e)
+        print("sending message failed", e)
         return
-    
+
     print(message)
 
     try:
         token = TELEGRAM_TOKEN
         chat_id = TELEGRAM_CHAT_ID
         bot = telegram.Bot(token=token)
-        bot.sendMessage(chat_id=chat_id, text=f'[{socket.gethostname()}]\n{message}')
+        bot.sendMessage(chat_id=chat_id, text=f"[{socket.gethostname()}]\n{message}")
     except KeyError:
-        print('TELEGRAM_TOKEN or TELEGRAM_CHAT_ID is not set')
+        print("TELEGRAM_TOKEN or TELEGRAM_CHAT_ID is not set")
         pass
     except Exception as e:
-        print('Sending message failed: ', e)
+        print("Sending message failed: ", e)
         return
 
 
