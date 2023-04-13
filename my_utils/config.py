d631005dc4200f7b09a6d8c7a7332f5965cb6898
Nikos Tsiknakis <tsiknakisn@gmail.com>
2023-04-13 08:45:01 +0300

    update

--- my_utils/config.py
+++ my_utils/config.py
@@ -65,6 +65,7 @@ def process_config(
     fold: int = None,
     mkdirs=True,
     config_copy=True,
+    version=None,
 ):
     print("Processing config..")
     config, _ = get_config(file)
@@ -93,13 +94,16 @@ def process_config(
         new_version = 0
         if os.path.exists(config.callbacks.tensorboard_log_dir):
             # Get experiment version
-            version = os_sorted(
-                glob.glob(
-                    os.path.join(config.callbacks.tensorboard_log_dir, "version_*")
+            if version is None:
+                version = os_sorted(
+                    glob.glob(
+                        os.path.join(config.callbacks.tensorboard_log_dir, "version_*")
+                    )
                 )
-            )
-            if len(version) > 0:
-                new_version = int(os.path.basename(version[-1]).split("_")[-1]) + 1
+                if len(version) > 0:
+                    new_version = int(os.path.basename(version[-1]).split("_")[-1]) + 1
+            else:
+                new_version = version
         config.callbacks.checkpoint_dir = os.path.join(
             base_dir, "checkpoints", f"version_{new_version}"
         )
